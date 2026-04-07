02717d359911f3fbddf6551c3a56b98236e21cfa
sagadan <112393331+sagadan@users.noreply.github.com>
2026-04-07 11:48:43 +0300

    lab02

---
+++ src/lab02/collection.py
@@ -0,0 +1,111 @@
+from model import Character 
+
+class CharacterCollection:
+    def __init__(self):
+        "Инициализация пустой коллекции"
+        self._characters = []
+
+
+    def _check_type(self, character):
+        "проверка типа добавляемого объекта"
+        if not isinstance (character, Character):
+            raise TypeError (f"Ошибка! Ожидается объект Chararacter, получен {type(character)._name_}")
+        
+    # Основные методы 
+
+    def add(self, character):
+        "добавить персонажа в коллекцию"
+        self._check_type(character)
+        if character in self._characters:
+            raise ValueError('Персонаж уже был добавлен')
+        self._characters.append(character)
+
+    def remove(self, character):
+        "удалить персонажа"
+        self._check_type(character)
+        self._characters.remove(character)
+
+    def remove_at(self, index):
+        if 0 <= index <= len(self._characters):
+            return self._chaarcters.pop(index)
+        raise IndexError (f"Индекс не попадает в диапазон от 0 до {len(self._characters)}")
+    
+    def get_all(self):
+        return self._characters 
+    
+
+    # Методы поиска
+    
+    def find_by_name (self, name: str):
+        "поиск персонажа по имени"
+        for character in self._characters:
+            if character.game_name.lower() == name.lower():
+                return character
+        return None
+    
+    def find_by_health_range(self, min_health: int = 0, max_health: int = 100):
+        "поиск персонажей в заданном диапазоне здоровья"
+        result = []
+        for character in self._characters:
+            if min_health <= character.health <= max_health:
+                result.append(character)
+        return result 
+    
+    def find_by_power_range (self, min_power: int = 1, max_power: int = 60):
+        "поиск персонажей в заданном диапазоне силы"
+        result = []
+        for character in self._characters:
+            if min_power <= character.power <= max_power:
+                result.append(character)
+        return result 
+    
+    # Фильтрация
+
+    def get_healthy (self, min_health: int = 50):
+        "возвращает новую коллекцию с здоровыми песронажами"
+        new_collection = CharacterCollection()
+        for character in self._characters:
+            if character.health >= min_health:
+                new_collection.add(character)
+        return new_collection 
+    
+    def get_strong (self, min_power: int = 30):
+        "возвращает новую коллекцию с сильными персонажами"
+        new_collection = CharacterCollection()
+        for character in self._characters:
+            if character.power >= min_power:
+                new_collection.add(character)
+        return new_collection 
+    
+
+    # Магические методы
+
+    def __str__(self):
+        "строковое представение коллекции для пользователей"
+        result = ""
+        for char in self._characters:
+            result += str(char) + "\n"
+        return result
+        
+    def __len__(self):
+        "возвращает количсетво персонажей в коллекции"
+        return len(self._characters)
+    
+    def __iter__(self):
+        "возвращает итератор по коллекции"
+        return iter(self._characters)
+    
+    def __getitem__(self, index):
+        "доступ к перонажу по индексу"
+        return self._characters[index]
+    
+
+            
+
+
+
+    
+
+        
+
+
---
+++ src/lab02/demo.py
@@ -0,0 +1,64 @@
+from collection import CharacterCollection
+from model import Character
+
+# Создание персонажей
+character1 = Character("Арагорн", 95, 35, 55, 45)
+character2 = Character("Гэндальф", 80, 25, 40, 75)
+character3 = Character("Леголас", 75, 38, 50, 60)
+character4 = Character("Фродо", 60, 20, 15, 40)
+character5 = Character("Гимли", 90, 30, 58, 30)
+character6 = Character("Саруман", 85, 28, 35, 78)
+
+print("> Добавление персонажей в коллекцию")
+character_collection = CharacterCollection()
+character_collection.add(character1)
+character_collection.add(character2)
+character_collection.add(character3)
+character_collection.add(character4)
+character_collection.add(character5)
+character_collection.add(character6)
+
+for char in character_collection:
+    print(f"   {char.game_name}, здоровье: {char.health}, сила: {char.power}")
+print(f"Добавлено {len(character_collection)} персонажей")
+
+
+print("\n> Коллекция после удаления персонажа")
+print(f"Удаляем: {character2.game_name}")
+character_collection.remove(character2)
+for char in character_collection:
+    print(f"   {char.game_name}, здоровье: {char.health}, сила: {char.power}")
+print(f"В коллекции осталось {len(character_collection)} персонажей")
+
+print("\n> Поиск персонажа по имени 'Гимли'")
+find_gimli = character_collection.find_by_name("Гимли")
+if find_gimli:
+    print(f"   {find_gimli.game_name}, здоровье: {find_gimli.health}, сила: {find_gimli.power}")
+
+print("\n> Поиск персонажей по силе (> 50)")
+find_strong = character_collection.find_by_power_range(min_power=51)
+for strong in find_strong:
+    print(f"   {strong.game_name}, сила: {strong.power}")
+
+
+print("\n> Обход коллекции с помощью iterator")
+print(f"Всего персонажей: {len(character_collection)}")
+for i, character in enumerate(character_collection, 1):
+    print(f"  {i}. {character.game_name}, здоровье: {character.health}, сила: {character.power}, интеллект: {character.intelligence}")
+
+print("\n> Проверка ошибки на добавление дубликата")
+try:
+    character_collection.add(character1)
+    print("Ошибка: дубликат был добавлен!")
+except ValueError as e:
+    print(f"Дубликат не добавлен так как: {e}")
+
+print("\n---Сценарий 1: фильтрация здоровых персонажей (здоровье >= 80)")
+healthy_characters = character_collection.get_healthy(min_health=80)
+
+print(f"\n1) Здоровых персонажей: {len(healthy_characters)} из {len(character_collection)}")
+
+print("\n2) Список здоровых персонажей:")
+for character in healthy_characters:
+    print(f"   {character.game_name}, здоровье: {character.health}")
+
